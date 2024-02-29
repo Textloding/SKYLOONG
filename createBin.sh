8df0bf8e7119d5fe79828d10e8709df1531972a2
lxydiy <1452206376@qq.com>
2024-02-29 12:46:12 +0800

    修复创建bin时参数错误

--- createBin.sh
+++ createBin.sh
@@ -4,5 +4,5 @@ esptool.py --chip esp32s3 merge_bin \
   --flash_mode dio \
   --flash_freq 80m \
   --flash_size 16MB \
-  0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x10000 build/GK87-Screen.bin
+  0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x10000 build/ota_data_initial.bin 0x20000 build/GK87-Screen.bin
   
