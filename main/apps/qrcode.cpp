2ce89ae811d8ab6d5a940cf3370433405d9759ad
白金颖 <baijinying@263.net>
2026-01-22 09:28:38 +0800

    在二维码下显示 url

--- main/apps/qrcode.cpp
+++ main/apps/qrcode.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static lv_point_t qrcode_line_points[] = {{58, 0}, {261, 0}, {261, 203}, {58, 203}, {58, 0}};
+static lv_point_t qrcode_line_points[] = {{63, 3}, {256, 3}, {256, 196}, {63, 196}, {63, 3}};
 
 void AppQRCode::setup() {
     char data[100];
@@ -33,8 +33,8 @@ void AppQRCode::setup() {
         lv_obj_set_style_line_color(line, lv_color_white(), 0);
         lv_line_set_points(line, qrcode_line_points, 5);
 
-        lv_obj_t *qrcode_obj = lv_qrcode_create(_appScreen, 200, lv_color_black(), lv_color_white());
-        lv_obj_set_pos(qrcode_obj, 60, 2);
+        lv_obj_t *qrcode_obj = lv_qrcode_create(_appScreen, 190, lv_color_black(), lv_color_white());
+        lv_obj_set_pos(qrcode_obj, 65, 5);
         snprintf(data, sizeof(data), "http://%s", WiFi.localIP().toString().c_str());
         lv_qrcode_update(qrcode_obj, data, strlen(data));
 
@@ -42,10 +42,19 @@ void AppQRCode::setup() {
         lv_obj_set_style_text_align(lbl_prompt, LV_TEXT_ALIGN_CENTER, 0);
         lv_obj_set_style_text_font(lbl_prompt, &lv_font_chinese_16, false);
         lv_obj_set_style_text_color(lbl_prompt, lv_color_white(), 0);
-        lv_obj_set_size(lbl_prompt, 300, 30);
+        lv_obj_set_size(lbl_prompt, 300, 20);
         snprintf(data, sizeof(data), _tr(I18N_ID_CONNECTED_WIFI), WiFi.SSID().c_str());
         lv_label_set_text(lbl_prompt, data);
-        lv_obj_set_pos(lbl_prompt, 10, 210);
+        lv_obj_set_pos(lbl_prompt, 10, 197);
+
+        lv_obj_t *lbl_url = lv_label_create(_appScreen);
+        lv_obj_set_style_text_align(lbl_url, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_set_style_text_font(lbl_url, &lv_font_chinese_16, false);
+        lv_obj_set_style_text_color(lbl_url, lv_color_white(), 0);
+        lv_obj_set_size(lbl_url, 300, 20);
+        snprintf(data, sizeof(data), "http://%s", WiFi.localIP().toString().c_str());
+        lv_label_set_text(lbl_url, data);
+        lv_obj_set_pos(lbl_url, 10, 215);
     }
 
     hal.UNLOCKLV();
