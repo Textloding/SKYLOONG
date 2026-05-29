93369ec400c7bd71682fcc0901a320b25fdd8dc5
白金颖 <baijinying@263.net>
2026-05-29 09:24:09 +0800

    提交 TFT_eSPI 组件代码

---
+++ components/TFT_eSPI/User_Setups/Setup168_ESP32_ST7789_Parallel.h
@@ -0,0 +1,40 @@
+#define USER_SETUP_ID 168
+
+#define ST7789_DRIVER
+
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 320
+
+#define TFT_RGB_ORDER TFT_BGR
+
+#define TFT_INVERSION_OFF
+
+#define TFT_PARALLEL_8_BIT
+
+#define TFT_CS 15
+#define TFT_DC 14
+#define TFT_RST 16
+
+#define TFT_WR 14
+#define TFT_RD 5
+
+#define TFT_D0 47
+#define TFT_D1 48
+#define TFT_D2 45
+#define TFT_D3 38
+#define TFT_D4 39
+#define TFT_D5 40
+#define TFT_D6 41
+#define TFT_D7 42
+
+#define TFT_BL 21
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SMOOTH_FONT
